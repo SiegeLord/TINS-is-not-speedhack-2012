7bcde0d62e8b875b8a99a604330ec2af9d68f8b6
SiegeLord <slabode@aim.com>
2012-08-09 17:36:19 -0400

    Fix the PeriodDragon build script

--- Ben Davis_Period Dragon/PeriodDragon/linux_build.sh
+++ Ben Davis_Period Dragon/PeriodDragon/linux_build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ldc2 *.d -ofperiod_dragon pkg-config --static --libs allegro_ttf-static-5.1 allegro_acodec-static-5.1 allegro_primitives-static-5.1 allegro_image-static-5.1 | sed -e 's/-[lL]/-L&/g'
+ldc2 *.d -ofperiod_dragon `pkg-config --static --libs allegro_ttf-static-5.1 allegro_acodec-static-5.1 allegro_primitives-static-5.1 allegro_image-static-5.1 allegro_memfile-static-5.1 | sed -e 's/-[lL]/-L&/g'`
